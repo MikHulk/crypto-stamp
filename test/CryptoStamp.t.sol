5cd98cdc5df5f819f0a4ab2109b3ae4e9bff1531
MV <m.viey@wanadoo.fr>
2024-02-04 16:27:39 +0100

    chore: initial cleanup

---
+++ test/CryptoStamp.t.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {CryptoStamp} from "../src/CryptoStamp.sol";
+
+contract CryptoStampTest is Test {}
