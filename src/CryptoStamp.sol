c5c0b05fb1a04c2c20aedeae0fbc1beb340a46b6
MV <m.viey@wanadoo.fr>
2024-02-04 19:46:08 +0100

    feat: contract 1st

--- src/CryptoStamp.sol
+++ src/CryptoStamp.sol
@@ -4,5 +4,31 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract CryptoStamp is ERC721 {
+    
+    uint256 private _nextTokenId;
+    mapping(uint256 tokenId => string) private _registry;
+    
     constructor() ERC721("CryptoStamp", "STMP") {}
+
+    function _setContent(uint256 tokenId, string calldata content)
+        internal
+    {
+        _registry[tokenId] = content;
+    }
+    
+    function depositContent(string calldata content)
+        public returns (uint256)
+    {
+        address author = msg.sender;
+        uint256 newId = _nextTokenId++;
+        _mint(author, newId);
+        _setContent(newId, content);
+        return newId;
+    }
+
+    function getContent(uint256 tokenId)
+        public view returns(string memory)
+    {
+        return _registry[tokenId];
+    }
 }
--- test/CryptoStamp.t.sol
+++ test/CryptoStamp.t.sol
@@ -4,4 +4,80 @@ pragma solidity ^0.8.20;
 import {Test, console2} from "forge-std/Test.sol";
 import {CryptoStamp} from "../src/CryptoStamp.sol";
 
-contract CryptoStampTest is Test {}
+contract CryptoStampTest is Test {
+
+    CryptoStamp contract_;
+    address author0 = makeAddr("author0");
+    address author1 = makeAddr("author1");
+
+    string shortContent = "Hello";
+    string mediumContent =
+        unicode"Un vieil étang\n"
+        unicode"Une grenouille qui plonge,\n"
+        unicode"Le bruit de l'eau.\n";
+    string longContent;
+
+    function setUp() public {
+        contract_ = new CryptoStamp();
+        longContent = vm.readFile("./test/public-opinion.txt");
+    }
+
+    function test_token() public {
+        assertEq(contract_.name(), "CryptoStamp");
+        assertEq(contract_.symbol(), "STMP");
+    }
+
+    function test_deposit_short_content() public {
+        string memory content = shortContent;
+        vm.prank(author0);
+        uint256 tokenId = contract_.depositContent(content);
+        assertEq(tokenId, 0);
+    }
+
+    function test_get_short_content() public {
+        string memory content = shortContent;
+        vm.prank(author0);
+        uint256 tokenId = contract_.depositContent(content);
+        string memory contentFromRepo = contract_.getContent(tokenId);
+        assertEq(content, contentFromRepo);
+    }
+
+    function test_deposit_medium_content() public {
+        string memory content = mediumContent;
+        vm.prank(author0);
+        uint256 tokenId = contract_.depositContent(content);
+        assertEq(tokenId, 0);
+    }
+
+    function test_get_medium_content() public {
+        string memory content = mediumContent;
+        vm.prank(author0);
+        uint256 tokenId = contract_.depositContent(content);
+        string memory contentFromRepo = contract_.getContent(tokenId);
+        assertEq(content, contentFromRepo);
+    }
+
+    function test_deposit_long_content() public {
+        string memory content = longContent;
+        vm.prank(author0);
+        uint256 tokenId = contract_.depositContent(content);
+        assertEq(tokenId, 0);
+    }
+
+    function test_get_long_content() public {
+        string memory content = longContent;
+        vm.prank(author0);
+        uint256 tokenId = contract_.depositContent(content);
+        string memory contentFromRepo = contract_.getContent(tokenId);
+        assertEq(content, contentFromRepo);
+    }
+
+    function test_content_owner() public {
+        vm.prank(author0);
+        uint256 tokenId = contract_.depositContent(mediumContent);
+        address owner = contract_.ownerOf(tokenId);
+        assertEq(owner, author0);
+        assertNotEq(owner, author1);
+    }
+
+}
