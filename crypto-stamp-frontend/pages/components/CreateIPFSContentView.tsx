a9501f21d89266323c8fed0033147d2662032104
MikV <m.viey@wanadoo.fr>
2024-02-14 01:04:59 +0100

    feat: IPFS view load file from host

--- crypto-stamp-frontend/pages/components/CreateIPFSContentView.tsx
+++ crypto-stamp-frontend/pages/components/CreateIPFSContentView.tsx
@@ -1,6 +1,31 @@
-import { ReactNode } from 'react';
-import { Text } from '@chakra-ui/react';
+import { ReactNode, useEffect, useState } from 'react';
+import { Button } from '@chakra-ui/react';
+import { buttonStyles } from '@/lib/commonStyles';
+import { ErrorMessage, SuccessMessage } from '@/components/feedback';
+
 
 export function CreateIPFSContentView(): ReactNode {
-  return <Text>Comming soon...</Text>;
+  const [file, setFile] = useState<null|File>(null);
+  const [message, setMessage] = useState<null | string[]>(null);
+  const [error, setError] = useState<null | string[]>(null);
+
+  const pickFile = () => {
+    async function accessFiles() {
+      // @ts-ignore
+      const [fileHandle] = await window.showOpenFilePicker();
+
+      return await fileHandle.getFile()
+    }
+
+    accessFiles().then(setFile, e => setError([e.message]));
+
+  };
+  console.log(file);
+  return (
+    <>
+      <Button {...buttonStyles} onClick={pickFile}>pick a file</Button>
+      {message && <SuccessMessage messages={message} onClose={() => setMessage(null)} />}
+      {error && <ErrorMessage messages={error} onClose={() => setError(null)} />}
+    </>
+  );
 }
