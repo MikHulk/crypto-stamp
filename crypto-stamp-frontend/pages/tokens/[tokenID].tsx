9c9a9a4ce3bd07033775eeb072018ad1fb0a77a1
MikV <m.viey@wanadoo.fr>
2024-02-16 22:40:19 +0100

    ref: move components

--- crypto-stamp-frontend/pages/tokens/[tokenID].tsx
+++ crypto-stamp-frontend/pages/tokens/[tokenID].tsx
@@ -1,9 +1,6 @@
 import {
-  Box,
   Heading,
-  HStack,
-  Spinner
-} from '@chakra-ui/react';
+  HStack} from '@chakra-ui/react';
 import { useRouter } from 'next/router'
 import { ReactNode, useEffect, useState } from 'react';
 import { usePublicClient } from 'wagmi';
@@ -11,53 +8,7 @@ import { usePublicClient } from 'wagmi';
 import { getContent, Content, getContentType } from '@/lib/etherUtils';
 import { ErrorMessage } from '@/components/feedback';
 import { ArrowLeftIcon } from '@chakra-ui/icons';
-
-
-function TextView({content} : {content: string}): ReactNode {
-  return (
-    <Box p={1} 
-      overflowY="auto"
-      height="30em"
-      fontSize="xs" 
-      w="100%" 
-      bgColor="#ebdcdc" 
-      color="#403636" 
-      border="solid #c5749f" 
-      boxShadow="0 0 10px #c51170"
-    >
-      <pre>{content}</pre>
-    </Box>
-  );
-}
-
-function IpfsView({cid} : {cid: string}): ReactNode {
-  const uri = "https://" + cid + ".ipfs.nftstorage.link/";
-  const [content, setContent] = useState<null | string>(null);
-
-  useEffect(
-    () => {
-      async function fetchDocument() {
-        const response = await fetch(uri);
-        setContent(await response.text());
-      }
-      fetchDocument();
-    },
-    []
-  );
-  return (content ? <TextView content={content} /> : <Spinner />);
-}
-
-function UnknownTypeView(): ReactNode {
-  return (
-    <Heading size="md">Unknown Type</Heading>
-  );
-}
-
-function getViews(type: string, content: string): ReactNode {
-  if(type === "Text") return  <TextView content={content} />;
-  if(type === "URI") return  <IpfsView cid={content} />;
-  else return <UnknownTypeView />;
-}
+import { TextView, IpfsView, UnknownTypeView } from './components';
 
 
 export default function Page(): ReactNode {
@@ -90,6 +41,12 @@ export default function Page(): ReactNode {
     [router]
   );
 
+  function getViews(type: string, content: string): ReactNode {
+    if(type === "Text") return  <TextView content={content} />;
+    if(type === "URI") return  <IpfsView cid={content} />;
+    else return <UnknownTypeView />;
+  }
+  
   return (
     <>
       <Heading size="md">TokenID: {tokenID}</Heading>
@@ -98,11 +55,11 @@ export default function Page(): ReactNode {
         <>
           <HStack justify="space-between" w="100%">
             <ArrowLeftIcon 
-              _hover={{cursor: "pointer"}} 
-              onClick={() => router.push("/")} 
+              _hover={ {cursor: "pointer"} } 
+              onClick={ () => router.push("/") } 
             />
             <Heading textAlign="center" flexGrow="1" size="xd">
-              Type: {getContentType(content.contentType)}
+              Type: { getContentType(content.contentType) }
             </Heading>
           </HStack>
           { getViews(getContentType(content.contentType), content.value) }
