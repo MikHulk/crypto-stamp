1b31dfff8fd6ded613469b7757860b918c864a5c
MikV <m.viey@wanadoo.fr>
2024-02-17 15:47:50 +0100

    feat: check chain configuraed at startup

--- crypto-stamp-frontend/pages/_app.tsx
+++ crypto-stamp-frontend/pages/_app.tsx
@@ -20,26 +20,45 @@ import { publicProvider } from 'wagmi/providers/public';
 
 import StyleIt from '@/lib/SyleIt';
 
-const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [
-    mainnet,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia, localhost] : []),
-  ],
-  [publicProvider()]
-);
 
-const { connectors } = getDefaultWallets({
-  appName: 'CryptoStamp',
-  projectId: '9998c9182ff85120dfa92a5925333d75',
-  chains,
-});
+function getConfig() {
+  function configure(chainString : string|undefined) {
+    const availableChains = {
+      "localhost": localhost,
+      "mainnet": mainnet,
+      "sepolia": sepolia
+    }
+    // @ts-ignore
+    const chain = availableChains[chainString];
+    if(chain) {
+      const { chains, publicClient, webSocketPublicClient } = configureChains(
+        [
+          chain,
+        ],
+        [publicProvider()]
+      );
+      
+      const { connectors } = getDefaultWallets({
+        appName: 'CryptoStamp',
+        projectId: '9998c9182ff85120dfa92a5925333d75',
+        chains,
+      });
+      
+      const wagmiConfig = createConfig({
+        autoConnect: true,
+        connectors,
+        publicClient,
+        webSocketPublicClient,
+      });
+      return [chains, wagmiConfig];
+    }
+    // comme le JS...
+    return [null, null];
+  }
+  return configure(process.env.NEXT_PUBLIC_CHAIN)
+}
 
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors,
-  publicClient,
-  webSocketPublicClient,
-});
+const [chains, wagmiConfig] = getConfig();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -51,45 +70,52 @@ function MyApp({ Component, pageProps }: AppProps) {
           name="description"
         />
       </Head>
-        <WagmiConfig config={wagmiConfig}>
-          <RainbowKitProvider theme={darkTheme()} chains={chains}>
-            <StyleIt>
-              <main>
-                <VStack 
-                  ml="auto"
-                  mr="auto"
-                  mt={10}
-                  p={5}
-                  color="#cca8c4"
-                  backgroundColor="gray.700"
-                  maxW={800}
+      <StyleIt>
+        <main>
+            { (chains && wagmiConfig) ? (
+              // @ts-ignore
+              <WagmiConfig config={wagmiConfig}>
+                <RainbowKitProvider 
+                  theme={darkTheme()}
+                  // @ts-ignore
+                  chains={chains}
                 >
-                  <VStack mb={7}>
-                    <Heading textShadow="2px 1px 7px #ce3385" color="#dd5f91">
-                      CryptoStamp
-                    </Heading>
-                    <Heading as="i" size="md">
-                      Protect your writting on the blockchain
-                    </Heading>
+                  <VStack 
+                    ml="auto"
+                    mr="auto"
+                    mt={10}
+                    p={5}
+                    color="#cca8c4"
+                    backgroundColor="gray.700"
+                    maxW={800}
+                  >
+                    <VStack mb={7}>
+                      <Heading textShadow="2px 1px 7px #ce3385" color="#dd5f91">
+                        CryptoStamp
+                      </Heading>
+                      <Heading as="i" size="md">
+                        Protect your writting on the blockchain
+                      </Heading>
+                    </VStack>
+                    <ConnectButton />
+                    <Component {...pageProps} />
                   </VStack>
-                  <ConnectButton />
-                  <Component {...pageProps} />
-                </VStack>
-              </main>
-              <footer>
-                <Flex 
-                  color="#bd7f9a" 
-                  borderTop="1px" 
-                  borderColor="#bd7f9a" 
-                  maxW={800} mr="auto" ml="auto" mt={10}
-                >
-                  <Spacer />
-                  <i>Mickaël Viey. Alyra 2024.</i>
-                </Flex>
-              </footer>
-            </StyleIt>
-          </RainbowKitProvider>
-        </WagmiConfig>
+                </RainbowKitProvider>
+              </WagmiConfig>
+            ) : <Heading color="red">No chain configured</Heading> }
+        </main>
+        <footer>
+          <Flex 
+            color="#bd7f9a" 
+            borderTop="1px" 
+            borderColor="#bd7f9a" 
+            maxW={800} mr="auto" ml="auto" mt={10}
+          >
+            <Spacer />
+            <i>Mickaël Viey. Alyra 2024.</i>
+          </Flex>
+        </footer>
+      </StyleIt>
     </>
   );
 }
